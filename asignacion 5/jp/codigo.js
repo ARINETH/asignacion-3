744ea0c33375c583733a592016249cdd1f3c6294
aribet <arebj22@gmail.com>
2018-06-07 15:30:23 -0400

    asignacion5 esin terminar Signed-off-by: aribet <arebj22@gmail.com>

---
+++ asignacion 5/jp/codigo.js
@@ -0,0 +1,30 @@
+ function sumar()
+ {
+	var x = parseInt(document.getElementById('n1').value);
+	var y = parseInt(document.getElementById('n2').value);
+	alert (x+y)
+	 }
+function restar()
+ {
+	var x = parseInt(document.getElementById('n1').value);
+	var y = parseInt(document.getElementById('n2').value); 
+	document.getElementById('igual').innerHTML = a-y;
+	}
+function multiplicar()
+ {
+	var x = parseInt(document.getElementById('n1').value);
+	var y = parseInt(document.getElementById('n2').value); 
+	document.getElementById('tname').innerHTML = a*y;
+	}
+function dividir()
+ {
+	var x = parseInt(document.getElementById('n1').value);
+	var y = parseInt(document.getElementById('n2').value); 
+	document.getElementById('tname').innerHTML = a/y;
+	}
+function calcular(){
+	var x = parseInt(document.getElementById('n1').value);
+	var y = parseInt(document.getElementById('n2').value); 
+	document.getElementById('igual').operaciones;
+		
+}
